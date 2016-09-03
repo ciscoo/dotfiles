9d04839be17bf62f7c7d9e3cfd34bccaeaa3ce07
Francisco Mateo <cisco@Franciscos-MacBook-Pro.local>
2016-09-03 12:38:38 -0500

    Added Homebrew of macOS.

---
+++ macos/homebrew.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+#
+# Homebrew
+#
+# Installs my perferred Homebrew packages.
+
+set -e
+
+# Check for Homebrew
+if test ! $(which brew); then
+  echo "Installing Homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+cd "$(dirname "$0")"
+
+TAPS=(
+  homebrew/binary
+  homebrew/dupes
+  homebrew/versions
+  caskroom/cask
+  caskroom/versions
+  caskroom/fonts
+)
+
+FORMULAS=(
+  git
+  optipng
+  python3
+  screenfetch
+  wget
+  zsh
+)
+
+FONTS=(
+  font-source-code-pro
+  font-sauce-code-powerline
+  font-roboto
+)
+
+CASKS=(
+  alfred
+  ccleaner
+  dropbox
+  flux
+  google-chrome
+  google-drive
+  intellij-idea
+  iterm2
+  java
+  keka
+  postbox
+  slack
+  sourcetree
+  spectacle
+  visual-studio-code
+  vlc
+)
+
+for tap in ${TAPS[@]}; do
+    brew tap $tap
+done
+
+brew update
+
+brew install ${FORMULAS[@]}
+brew cask install ${CASKS[@]}
+brew cask install ${FONTS[@]}
+
+brew cleanup
+brew cask cleanup
+
+exit
